14cdad27c149d38782637224a83d179460db0716
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-11-12 20:32:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,70 +1,14 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const dotenv = require("dotenv"); 
-const bodyParser = require("body-parser"); 
-const Cors = require("cors");   
-const passport = require("passport")
-var router = require('./routes/index'); 
-var Authrouter = require('./routes/Auth'); 
-const session = require("express-session"); 
-const Sanitize = require('./middleware/Sanitize');
+const express = require('express')
+const app = express()
+const port = 3000
 
-var app = express();
-dotenv.config({
-  path:path.join(__dirname, './.env')
-});
-
- 
-app.use(session({secret:"eduallapp", cookie: { maxAge:24 * 60 * 60 * 1000}}))
-
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(logger('dev'));
-app.use(express.json()); 
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(Cors({
-  credentials:true, 
-  origin:'http://localhost:3000', 
-  methods:'GET,POST,DELETE,PUT'
- }));
-
- 
- /*
- app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE, OPTIONS');
-  next();
-});
-*/
-
-
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.use(router);
-app.use(Authrouter);
-app.use(Sanitize());
-app.use('/images', express.static(__dirname+'/images'));
-app.use('/assets', express.static(__dirname+'/assets'));
-
- 
-
-app.listen(process.env.PORT , function () {
-  console.log("Server started at port: 5000");
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-module.exports = app;
-
-
-
- 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
 

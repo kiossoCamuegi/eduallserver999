3c362579b5233431744e96b0bd64b9ed4ff7b87f
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-11-13 07:19:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,8 @@
-var createError = require('http-errors');
 var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const dotenv = require("dotenv"); 
-const bodyParser = require("body-parser"); 
-const Cors = require("cors");   
-const passport = require("passport")
 var router = require('./routes/index');  
-const session = require("express-session"); 
-
 var app = express();
-dotenv.config({
-  path:path.join(__dirname, './.env')
-});
-
  
-app.use(session({secret:"eduallapp", cookie: { maxAge:24 * 60 * 60 * 1000}}))
-
-app.use(logger('dev'));
 app.use(express.json()); 
-app.use(cookieParser());
- 
-
-
-app.use(express.json());
 app.use(router); 
  
 

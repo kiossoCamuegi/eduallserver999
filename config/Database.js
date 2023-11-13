fe8330fe2c1451c2db8d9b288b5e874031e87f1c
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-11-13 08:30:09 +0100

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,15 @@
 
 const mysql = require("mysql"); 
 
-const CurrentNetworkStatus = ()=>{  
- return {host:"bbwmy0j6vnqfwlwreg3x-mysql.services.clever-cloud.com",  user:"uf3c2i1lgdfrfn9v",
-  password:"mY92miw96iMOuJHuWXH9",   database:"bbwmy0j6vnqfwlwreg3x", port:3306};
-} 
  
-let DATABASE = mysql.createConnection(CurrentNetworkStatus);
+ 
+let DATABASE = mysql.createConnection({
+  host:"bbwmy0j6vnqfwlwreg3x-mysql.services.clever-cloud.com", 
+  user:"uf3c2i1lgdfrfn9v",
+  password:"mY92miw96iMOuJHuWXH9",   
+  database:"bbwmy0j6vnqfwlwreg3x", 
+  port:3306
+});
 module.exports =  {DATABASE};
  
  

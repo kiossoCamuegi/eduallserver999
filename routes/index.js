9e391a6229fd02ae2ef3e75c5cad9a336f585785
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-11-13 07:24:44 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express"); 
 const router = express.Router();
- 
+ const {GetStudents} = require("../Controllers/Students");
+
 router.get("/", (req, res)=>{
    res.send("Hello my greatest friennds !");
 })
+
+router.get('/eduallstudentsapi/get', GetStudents);
 
 module.exports =  router;

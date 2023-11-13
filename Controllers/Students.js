ed4c99bfb69cf70fc707ee6e231db15dbe612780
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-11-13 07:42:35 +0100

    Update Students.js

--- Controllers/Students.js
+++ Controllers/Students.js
@@ -5,6 +5,7 @@ const { DATABASE } = require("../config/Database");
    LEFT JOIN eduall_cicles ON ed_cicle_id = eduall_class.ed_class_cicle  WHERE ed_student_deleted = 0  ORDER BY ed_student_name ASC`;
    const PARAMS = [];
      DATABASE.query(query, PARAMS, (err, rows)=>{ 
+          console.log(err)
         if(err) return res.status(400).json({status:300, success:false, msg:err});
         return res.json(rows);
      });

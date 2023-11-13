d52833e731f3d0f2c5378f0c4dcd0e08e7c1dab6
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-11-13 07:15:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const dotenv = require("dotenv");
 const bodyParser = require("body-parser"); 
 const Cors = require("cors");   
 const passport = require("passport")
-var router = require('./routes/index'); 
-var Authrouter = require('./routes/Auth'); 
+var router = require('./routes/index');  
 const session = require("express-session"); 
-const Sanitize = require('./middleware/Sanitize');
 
 var app = express();
 dotenv.config({
@@ -20,41 +18,17 @@ dotenv.config({
  
 app.use(session({secret:"eduallapp", cookie: { maxAge:24 * 60 * 60 * 1000}}))
 
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(logger('dev'));
 app.use(express.json()); 
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(Cors({
-  credentials:true, 
-  origin:'https://eduallsys.com', 
-  methods:'GET,POST,DELETE,PUT'
- }));
-
  
- /*
- app.use((_req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE, OPTIONS');
-  next();
-});
-*/
 
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
-app.use(router);
-app.use(Authrouter);
-app.use(Sanitize());
-app.use('/images', express.static(__dirname+'/images'));
-app.use('/assets', express.static(__dirname+'/assets'));
-
+app.use(router); 
  
 
-app.listen(process.env.PORT , function () {
+app.listen(5000 , function () {
   console.log("Server started at port: 5000");
 })
 

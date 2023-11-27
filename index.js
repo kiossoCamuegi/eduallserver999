b48623c466fe53dd0fccd97b05e8eaab2e3a4851
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-11-27 07:50:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,94 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var cookieParser = require('cookie-parser'); 
 const dotenv = require("dotenv"); 
 const bodyParser = require("body-parser"); 
 const Cors = require("cors");   
 const passport = require("passport")
 var router = require('./routes/index');  
 var Authrouter = require('./routes/Auth'); 
-const session = require("express-session"); 
-const Sanitize = require('./middleware/Sanitize');
+const session = require("express-session");  
+const Sanitize = require('./middleware/Sanitize'); 
+const MySQLStore = require('express-mysql-session-ci')(session);
+ 
 
 
-
-    
-const http = require("http");
-const { Server } = require("socket.io"); 
-
-
-
-var AppServer = express();
+var app = express();
 dotenv.config({
   path:path.join(__dirname, './.env')
 });
 
- 
-AppServer.use(session({secret:"eduallapp", cookie: { maxAge:24 * 60 * 60 * 1000}}))
 
-
-
-AppServer.use(passport.initialize());
-AppServer.use(passport.session());
-AppServer.use(logger('dev'));
-AppServer.use(express.json()); 
-AppServer.use(cookieParser());
-AppServer.use(express.static(path.join(__dirname, 'public')));
-AppServer.use(Cors({
-  credentials:true, 
-  origin:'https://eduallsys.com', 
-  methods:'GET,POST,DELETE,PUT'
- }));
-
+app.use(passport.initialize()); 
+app.use(Cors({credentials:true,  origin:'https://eduallsys.com', methods:'GET,POST,DELETE,PUT'}));
+app.use(express.json()); 
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
  
 
-AppServer.use(express.json());
-AppServer.use(bodyParser.urlencoded({extended:true}));
-AppServer.use(bodyParser.json());
-AppServer.use(router);
-AppServer.use(Authrouter); 
-AppServer.use(Sanitize());
-AppServer.use('/images', express.static(__dirname+'/images'));
-AppServer.use('/assets', express.static(__dirname+'/assets'));
-
+const options = {
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'eduall',
+  createDatabaseTable: true,
+};
  
+const sessionStore = new MySQLStore(options);
 
-const app = http.createServer(AppServer);
-const io = new Server(app); 
-
-io.on("connection", (socket) => {
-  console.log(`a user connected ${socket.id}`);
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore,
+  maxAge:1000000,
+  expires: 1000000
+}));
   
-  socket.on("send_message", (data) => {
-    console.log(data)
-    socket.broadcast.emit("receive_message", data);
-  });
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+ 
+
+
+
+
+
+
+
+
+
+app.use((req, res, next)=>{
+   console.log(req.session);
+   next();
 });
-  
+
+
+app.use(router);
+app.use(Authrouter); 
+app.use(Sanitize());
+app.use('/images', express.static(__dirname+'/images'));
+app.use('/assets', express.static(__dirname+'/assets'));
+
+ 
+ 
+ 
+ 
 
 app.listen(process.env.PORT , function () {
   console.log("Server started at port: 5000");
 })
 
-module.exports = app;
+module.exports = {app, sessionStore};
 
 
 
+ 
+
+
+
+ 
  
 
 

dbae22f90c177a45efca10730f8caa67f6a94655
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-11-27 08:14:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,11 @@ app.use(express.static(path.join(__dirname, 'public')));
  
 
 const options = {
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'eduall',
+ host:"bbwmy0j6vnqfwlwreg3x-mysql.services.clever-cloud.com",
+ user:"uf3c2i1lgdfrfn9v",
+ password:"mY92miw96iMOuJHuWXH9",  
+ database:"bbwmy0j6vnqfwlwreg3x",
+  port:3306,
   createDatabaseTable: true,
 };
  

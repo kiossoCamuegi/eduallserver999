609562220fe7479ae205467a0881d0ddeee6aec1
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-11-29 03:39:43 +0100

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -19,19 +19,15 @@ const DB_SQLITE = new sqlite.Database("./me.db", (err) => {
 
   
 const CurrentNetworkStatus = ()=>{  
-  return  {
-    connectionLimit: 10,
-    password:"",
-    user:"root",
-    database:"eduall",
-    host:"localhost",
-    port: process.env.DB_PORT,
-    charset:"utf8mb4"
- } 
-/*
   return {host:"bbwmy0j6vnqfwlwreg3x-mysql.services.clever-cloud.com",  user:"uf3c2i1lgdfrfn9v",
-  password:"mY92miw96iMOuJHuWXH9",   database:"bbwmy0j6vnqfwlwreg3x", port:3306};
-*/
+  password:"mY92miw96iMOuJHuWXH9",   database:"bbwmy0j6vnqfwlwreg3x", port:3306,
+ 
+    connectionLimit: 10 ,
+    connectTimeout: 10000,
+    waitForConnections: true,
+    queueLimit: 0 , 
+};
+
 } 
 
 const pool = mysql.createPool(CurrentNetworkStatus());

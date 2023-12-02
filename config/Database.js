e86828334a413935dd231f90fab6e731dfbe211b
Kiosso camuegi <72392628+kiossoCamuegi@users.noreply.github.com>
2023-12-02 11:26:09 +0100

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -54,7 +54,7 @@ console.log('db error', err);
 if(err.code === 'PROTOCOL_CONNECTION_LOST') {  
   handleDisconnect();      
 } else {    
-  throw err;              
+  handleDisconnect();            
 }
 });
 DATABASE  = connection;
